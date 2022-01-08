1879b0e8d19b1dad0c5c86a462f300506b5af136
one-summers-day <jiro.horikoshi1@gmail.com>
2022-01-07 18:13:52 -0600

    Implements multiple webpacks

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,11 +1,10 @@
 const path = require('path');
-const CompressionPlugin = require('compression-webpack-plugin');
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  plugins: [new CompressionPlugin],
-  mode: 'production',
+  mode: 'development',
+  devtool: 'source-map',
   entry: `${SRC_DIR}/index.js`,
   output: {
     filename: 'bundle.js',
